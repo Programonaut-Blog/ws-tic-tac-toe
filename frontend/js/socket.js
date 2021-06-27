f2ed3abcafa0eb3ad2dc3c11303a4b9745c9ccbd
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2021-06-27 22:26:31 +0200

    implement frontend

---
+++ frontend/js/socket.js
@@ -0,0 +1,77 @@
+const socket = io("ws://localhost:3000");
+const token = {
+    1: "cross",
+    2: "circle"
+};
+let clientId;
+let activeId;
+
+// get client id
+socket.on('clientId', (id) => {
+    clientId = id;
+})
+
+// get the first active player id and set up html for game
+socket.on('start', (startId) => {
+    activeId = startId;
+
+    document.getElementById('current-turn').classList.remove('hide');
+    document.getElementById('clientId').innerHTML = clientId == activeId ? 'your' : 'not your';
+});
+
+// get the active player and get field state
+socket.on('continue', (active, field) => {
+    activeId = active;
+    for (let x = 0; x < field.length; x++) {
+        for (let y = 0; y < field.length; y++) {
+            setField(x, y, field[y][x]);
+        }
+    }
+
+    document.getElementById('current-turn').classList.remove('hide');
+    document.getElementById('clientId').innerHTML = clientId == activeId ? 'your' : 'not your';
+})
+
+// update field with turn information
+socket.on('turn', (turn) => {
+    const {x, y, next} = turn;
+    setField(x, y, activeId);
+
+    activeId = next;
+    document.getElementById('clientId').innerHTML = clientId == activeId ? 'your' : 'not your';
+})
+
+// show popup with win information
+socket.on('over', (overObj) => {
+    winnerId = overObj['id']
+    if (winnerId != 0)
+        document.getElementById('winnerId').innerHTML = clientId == winnerId ? 'won' : 'lost';
+    else
+        document.getElementById('winnerId').innerHTML = 'draw';
+    
+    socket.disconnect();
+
+    document.getElementById('popup').classList.remove('hide');
+    document.getElementById('current-turn').classList.add('hide');
+})
+
+
+// send turn event to server
+function turn(x, y) {
+    if (activeId != clientId) return;
+    socket.emit("turn", {
+        "x": x,
+        "y": y
+    })
+}
+
+// update css for field
+function setField(x, y, id) {
+    let field = document.getElementById(`x${x}y${y}`)
+    field.classList.add(`${token[id]}`);
+}
+
+// restart the game
+function restart() {
+    window.location.reload();
+}
